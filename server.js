aedae387f59b80fc3d991661cc20475eacb29b06
rupesh kumar <149963939+ru-kumar@users.noreply.github.com>
2025-07-21 18:17:08 +0530

    Initial commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: process.env.CLIENT_URL||'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json());
